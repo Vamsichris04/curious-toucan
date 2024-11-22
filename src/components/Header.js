1caab475fd9ef7cdf681577b376d084da80fb766
celliot <celliot@msoe.edu>
2024-11-22 17:46:38 -0600

    Change readMe and change header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 // src/components/Header.js
 import React from "react";
+import '../styles/Header.css'; // If Header.css is in a 'styles' folder within 'src'
 
 function Header() {
   return (
     <header className="header">
-      <h1>DISCOVERY <span className="world">WORLD</span></h1>
+      <h1>DISCOVERY<span className="world">WORLD</span></h1>
     </header>
   );
 }
